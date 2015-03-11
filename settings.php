1e3d9dc340264089b64245564dbccffefc05383a
Ankit Agarwal <ankit@moodle.com>
2015-03-11 11:27:59 +0530

    Make report global - just like internet!

---
+++ settings.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Links and settings
+ *
+ * This file contains links and settings used by report_analytics
+ *
+ * @package    report_analytics
+ * @copyright  2015 onwards Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+// Just a link to course report.
+$ADMIN->add('reports', new admin_externalpage('reportanalytics', get_string('pluginname', 'report_analytics'),
+        "$CFG->wwwroot/report/analytics/index.php", 'report/analytics:view'));
+
+// No report settings.
+$settings = null;
